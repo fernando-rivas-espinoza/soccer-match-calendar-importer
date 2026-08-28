8c4d779aba4325665e7050d460ec4bfe96013424
fernando-re24 <fernandorivasespinoza24@gmail.com>
2026-08-28 00:09:52 -0400

    feat SCI-03: Added pruning logic to extract only necessary fixture information from API response and added a parameter to handle initial runs and regular refreshes.

--- src/fetch.py
+++ src/fetch.py
@@ -1,11 +1,43 @@
 from .api_request import make_request
 from datetime import datetime, timedelta, timezone
 
-def fetch_fixtures(team: str):
-    today = datetime.now(timezone.utc).date()
-    window_end = today + timedelta(weeks=2)
+def extract_schedule(Response, team_id):
+    pruned = {}
+    for match in Response["matches"]:
+        matchdate = match["utcDate"]
+        pruned["matchday"] = matchdate
+        curr_match = pruned[matchdate]
+        curr_match["matchday"] = match["season"]["currentMatchday"]
 
-    return make_request(
-        f"/teams/{team}/matches",
-        {"status": "SCHEDULED", "dateFrom": today, "dateTo": window_end},
-    )
+        if match["homeTeam"]["id"] != team_id:
+            curr_match["venue_status"] = "away"
+            curr_match["opponent_name"] = match["homeTeam"]["name"]
+            curr_match["opponent_id"] = match["homeTeam"]["id"]
+        else:
+            curr_match["venue_status"] = "home"
+            curr_match["opponent_name"] = match["awayTeam"]["name"]
+            curr_match["opponent_id"] = match["awayTeam"]["id"]
+
+        curr_match["competition_name"] = match["competition"]["name"]
+        curr_match["competition_id"] = match["competition"]["id"]
+
+    return pruned
+
+def fetch_fixtures(team_id: str, initial_run: bool):
+
+    if initial_run:
+        response = make_request(
+            f"/teams/{team_id}/matches",
+            {"status": "SCHEDULED"},
+        )
+        
+    else:
+        today = datetime.now(timezone.utc).date()
+        window_end = today + timedelta(weeks=2)
+
+        response = make_request(
+            f"/teams/{team_id}/matches",
+            {"status": "SCHEDULED", "dateFrom": today, "dateTo": window_end},
+        )
+
+    return extract_schedule(Response=response)
