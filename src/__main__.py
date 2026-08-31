fdb67dd10419fc3d7e7c3778ef061f0ed987ff89
Fernando <fernandorivasespinoza24@gmail.com>
2026-08-31 14:57:39 -0400

    feat/SCI-04-implement-translate (#7)
    * feat SCI-01: Rought draft of data fetching script.
    
    * fix SCI-01: Fixed datetime delta in season calculation.
    
    * fix SCI-01: Fixed season calculation again and added error checks for api call.
    
    * fix SCI-01: Made date calculation based on UTC.
    
    * fix SCI-01: Pivot to football-data.org api with wokring request
    
    * feat SCI-02: Refactored file structure in src.
    
    * fix/feat SCI-02: Fixed bugs and added error checks and output dumps.
    
    * chore SCI-02: readme update
    
    * feat SCI-03: Added pruning logic to extract only necessary fixture information from API response and added a parameter to handle initial runs and regular refreshes.
    
    * chore SCI-03: Added tests to git.
    
    * fix SCI-03: Fixed typos and issues in fetch.py.
    
    * fix SRC-03: Added logic to use 'La Liga' as the name for the spanish first division.
    
    * fix SCI-03: Changed the key for a fixture to be the match id from the api response to have stable uids.
    
    * feat SCI-04: Rough draft of translate.py
    
    * fix SCI-04: Fixed faulty translate logic.

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,32 @@
 from .fetch import fetch_fixtures
 from .models import get_teams
+from .translate import write_calendar
 import json
 from pathlib import Path
 
 def main():
+
+    team = "FC Barcelona"
+   
     if not Path("european_teams.json").is_file():
         with open("european_teams.json", "w") as file:
             json.dump(get_teams(), file, indent=2, ensure_ascii=False)
+            
+
+    with open("european_teams.json", "r") as file:
+        teams_dict = json.load(file)
+
+    team_id =  teams_dict["la_liga"]["FC Barcelona"]
 
     if not Path("teams_schedule.json").is_file():
             with open("teams_schedule.json", "w") as file:
-                json.dump(fetch_fixtures(team_id = "81", initial_run=True), file, indent=2, ensure_ascii=False)
+                json.dump(fetch_fixtures(team_id = team_id, initial_run=True), file, indent=2, ensure_ascii=False)
 
+    with open('teams_schedule.json', 'r') as file:
+        fixtures_dict = json.load(file)
+
+    write_calendar(fixtures_dict, team_name=team)
+    
 
 if __name__ == "__main__": 
     main()
